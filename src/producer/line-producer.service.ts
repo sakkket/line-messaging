d5bc2d29088fa1c34ff1d61a7177a1c6a80f1d09
Saket <saket@Sakets-MacBook-Air.local>
2026-06-20 15:52:41 +0530

    Added user logs

--- src/producer/line-producer.service.ts
+++ src/producer/line-producer.service.ts
@@ -5,6 +5,7 @@ import { LlmResponseService } from 'src/llm/llm-response.service';
 
 @Injectable()
 export class LineProducerService {
+  private readonly logger = new Logger(LineProducerService.name);
 
   constructor(
     private readonly llmResponse: LlmResponseService
@@ -13,8 +14,9 @@ export class LineProducerService {
   async createReplyMessages(event: webhook.Event): Promise<messagingApi.Message[]> {
     if (event.type === 'message' && event.message.type === 'text') {
       const userId = event.source?.userId ?? 'unknown';
+      this.logger.log(`Sending user query:: ${event.message.text} to LLM userId:: ${userId}`);
       const llmResponse:string = await this.llmResponse.generateResponse(event.message.text);
-
+      this.logger.log(`Received Response :: ${llmResponse} from LLM userId:: ${userId} user query:: ${event.message.text}`);
       return [
         {
           type: 'text',
